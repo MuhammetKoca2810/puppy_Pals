9150fccfb9718200469ecc52cb57d6932ea638fe
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-06-12 12:48:42 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,39 @@ import { puppyList } from "./data.js";
 import { useState } from "react";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handleClick = (id) => {
+    setFeatPupId(id);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("featuredPup: ", featuredPup);
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
-      })}
+      <div className="puppy-list">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              className="puppy-item"
+              onClick={() => handleClick(puppy.id)}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+      {featPupId && (
+        <div className="featured-puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
