1c6e88d0f568484e99881144ad64d89d96ff51b2
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-06-12 11:19:20 -0500

    Puppy Data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import { puppyList } from "./data.js";
+import { useState } from "react";
 function App() {
-  return <div></div>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
